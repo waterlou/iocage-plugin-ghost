2932d4f71605cfa304f17e7802479c79a2acac26
Water Lou <water@waterworld.com.hk>
2020-04-27 00:11:57 +0800

    try implement settings.json

--- overlay/post_install/70_ghost.sh
+++ overlay/post_install/70_ghost.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 GHOST_PATH=/usr/local/share/ghost
+LOG_PATH=/var/log/ghost
 
 # Install node packages
 npm -g install ghost-cli
 
-# Use ghost cli to install source
+mkdir -p ${LOG_PATH}
+chown www:www ${LOG_PATH}
+
 mkdir -p ${GHOST_PATH}
 cd ${GHOST_PATH}
 
@@ -21,18 +24,21 @@ PASS=`sysrc -f ${CFG} -n mysql_pass`
 
 HOST=`sysrc -n hostname`
 
-# create config file
+# install and create config file
 ghost install --db mysql --no-prompt --no-stack --no-setup
 ghost config --ip 0.0.0.0 --port 2368 --no-prompt --db mysql \
   --dbhost=localhost --dbuser=${USER} --dbpass=${PASS} --dbname=${DB} \
   --url https://${HOST}.local
 ghost config set database.connection.socketPath /tmp/mysql.sock
 ghost config set process local
+ghost config set logging.path /var/log/ghost/
 
+# change owner
 chown -R www:www ${GHOST_PATH}
 
 # Configure
 # sysrc ghost_enable=yes
+sysrc ghost_url=http://localhost
 
 # Start service
 service ghost start
